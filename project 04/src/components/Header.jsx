3d951023686c8d074b9b01f93fd691b0eb2404f0
LucianS7 <luciansabau87@yahoo.com>
2023-10-20 23:22:59 +0800

    new react project

---
+++ project 04/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function Header() {
+    return (
+      <header className="header">
+        <img className="header--image" src="../public/images/troll-face.png" alt="meme-image" />
+        <h3 className="header--title">MemeGenerator</h3>
+        <p className="header--project">React Course - Project 3</p>
+      </header>
+    )
+  }
+
+
